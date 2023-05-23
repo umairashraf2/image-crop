ec1c844af977935f6366295cf231f4715b489cab
Umair Ashraf <umair.dco@gmail.com>
2023-05-23 14:34:28 +0500

    new branch

--- static/js/main.js
+++ static/js/main.js
@@ -9,38 +9,32 @@ document.addEventListener("DOMContentLoaded", function () {
 	const horizontalCheckbox = document.getElementById("horizontalCheckbox");
 	const verticalRotate = document.getElementById("verticalRotate");
 	const horizontalRotate = document.getElementById("horizontalRotate");
+	const main_container = document.getElementById("main_container");
 	const preview = document.getElementById("preview");
 	const demo1 = document.getElementById("demo1");
+	const demo2 = document.getElementById("demo2");
 	const dwn = document.getElementById("dwn");
 	const dwn2 = document.getElementById("dwn2");
 	const grayscaleToggle = document.getElementById("grayscale-toggle");
 	const previewImg = document.getElementById("preview-img");
-	// const closeModal = document.getElementById("close-modal");
 	const y = document.getElementById("showContainer");
-	// const images = document.querySelectorAll(".card img");
-	// console.log(imadges.src);
-	// const images = document.querySelectorAll(".product--box");
-	// const cropperImg = document.getElementById("cropper-img");
-	// const cropperModal = document.getElementById("cropper-modal");
-	// const cropperModalObject = new bootstrap.Modal(
-	// 	document.getElementById("cropper-modal")
-	// );
+
 	let cropper;
+	let w;
+	let h;
 
-	// const urlParams = new URLSearchParams(window.location.search);
-	// const imgSrc = "./static/images/" + urlParams.get("img");
+	var urlParams = new URLSearchParams(window.location.search);
+	var imgSrc = urlParams.get("img");
 
-	// // Set image source
-	// image.src = imgSrc;
+	image.src = "static/images/" + imgSrc;
 
-	// Initialize Cropper.js on the image
-	// var croppers = new Cropper(document.getElementById("image"));
-
-	// if (cropper) cropper.destroy();
+	// const reader = new FileReader();
+	// reader.onload = (e) => {
+	// image.src = e.target.result;
+	if (cropper) cropper.destroy();
 	cropper = new Cropper(image, {
-		viewMode: 2,
+		viewMode: 1,
 		autoCropArea: 1,
-		guides: true,
 		// aspectRatio: 1,
 		// viewMode: 1,
 		// autoCropArea: 1,
@@ -51,140 +45,58 @@ document.addEventListener("DOMContentLoaded", function () {
 		// background color
 		// aspectRatio: NaN,
 		crop: function (event) {
-			let numLines = Math.round(event.detail.width / 37);
-
-			// Get a reference to the overlay
-			let overlay = document.querySelector(".cropper-crop-box");
-
-			// Clear any existing lines
-			// overlay.innerHTML = "";
-			overlay.querySelectorAll(".vertical-line").forEach((e) => {
-				e.remove();
-			});
-			// remove dashed lines
-			overlay.querySelectorAll(".cropper-dashed").forEach((e) => {
-				e.remove();
-			});
-
-			console.log(overlay);
-			let percent = 100 / numLines;
-
-			// Create the lines
-			for (let i = 0; i < numLines - 1; i++) {
-				// -1 because we don't need a line at the very right edge
-				// Calculate the left position of the line
-				let linePosition = percent * (i + 1); // +1 because we don't need a line at the very left edge
-
-				// Create a new div element for the line
-				let line = document.createElement("div");
-				line.classList.add("vertical-line");
-				line.style.position = "absolute";
-				line.style.left = linePosition + "%";
-				line.style.top = "0";
-				line.style.bottom = "0";
-				line.style.width = "2px";
-				line.style.backgroundColor = "white"; // Use whatever color you like
-
-				// Add the line to the overlay
-				overlay.appendChild(line);
-			}
-			demo1.innerHTML = Math.round(event.detail.width / 37) "m²" +
-				Math.round(event.detail.width / 37) +
-				" x " +
-				Math.round(event.detail.height / 37) +
-				" cm";
+			demo1.innerHTML =
+				"Width: " +
+				Math.round(event.detail.width) +
+				"px," +
+				" Height: " +
+				Math.round(event.detail.height) +
+				"px, " +
+				"image quality: 100%";
 		},
 	});
-	cropper.options.guides = {
-		lines: 10,
-	};
+	// };
 
-	inputImage.addEventListener("change", (e) => {
-		console.log("123");
-		const file = e.target.files[0];
-		if (file.size > 5 * 1024 * 1024) {
-			// 5 MB in bytes
-			alert("Please upload an image smaller than 5 MB.");
-			return;
-		}
-		if (file) {
-			const reader = new FileReader();
-			reader.onload = (e) => {
-				image.src = e.target.result;
-				if (cropper) cropper.destroy();
-				cropper = new Cropper(image, {
-					viewMode: 1,
-					autoCropArea: 1,
-					// aspectRatio: 1,
-					// viewMode: 1,
-					// autoCropArea: 1,
-					// cropBoxMovable: false,
-					// cropBoxResizable: false,
-					// zoomOnTouch: false,
-					// zoomOnWheel: false,
-					// background color
-					// aspectRatio: NaN,
-					crop: function (event) {
-						let numLines = 10;
-
-						// Get the width of the crop box in pixels
-						let cropBoxWidth = event.detail.width;
-
-						// Calculate the distance between each line
-						let lineDistance = cropBoxWidth / numLines;
-
-						// Get a reference to the overlay
-						let overlay =
-							document.querySelector(".cropper-crop-box");
-
-						// Clear any existing lines
-						// overlay.innerHTML = "";
-						overlay
-							.querySelectorAll(".vertical-line")
-							.forEach((e) => {
-								e.remove();
-							});
-						// remove dashed lines
-						overlay
-							.querySelectorAll(".cropper-dashed")
-							.forEach((e) => {
-								e.remove();
-							});
-
-						console.log(overlay);
-
-						// Create the lines
-						for (let i = 0; i < numLines - 1; i++) {
-							// -1 because we don't need a line at the very right edge
-							// Calculate the left position of the line
-							let linePosition = lineDistance * (i + 1); // +1 because we don't need a line at the very left edge
-
-							// Create a new div element for the line
-							let line = document.createElement("div");
-							line.classList.add("vertical-line");
-							line.style.position = "absolute";
-							line.style.left = linePosition + "px";
-							line.style.top = "0";
-							line.style.bottom = "0";
-							line.style.width = "1px";
-							line.style.backgroundColor = "red"; // Use whatever color you like
-
-							// Add the line to the overlay
-							overlay.appendChild(line);
-						}
-						demo1.innerHTML =
-							"Width: " +
-							Math.round(event.detail.width / 37) +
-							"cm" +
-							" Height: " +
-							Math.round(event.detail.height / 37) +
-							"cm";
-					},
-				});
-			};
-			reader.readAsDataURL(file);
-		}
-	});
+	// inputImage.addEventListener("change", (e) => {
+	// 	console.log("123");
+	// 	const file = e.target.files[0];
+	// 	if (file.size > 5 * 1024 * 1024) {
+	// 		// 5 MB in bytes
+	// 		alert("Please upload an image smaller than 5 MB.");
+	// 		return;
+	// 	}
+	// 	if (file) {
+	// 		const reader = new FileReader();
+	// 		reader.onload = (e) => {
+	// 			image.src = e.target.result;
+	// 			if (cropper) cropper.destroy();
+	// 			cropper = new Cropper(image, {
+	// 				viewMode: 1,
+	// 				autoCropArea: 1,
+	// 				// aspectRatio: 1,
+	// 				// viewMode: 1,
+	// 				// autoCropArea: 1,
+	// 				// cropBoxMovable: false,
+	// 				// cropBoxResizable: false,
+	// 				// zoomOnTouch: false,
+	// 				// zoomOnWheel: false,
+	// 				// background color
+	// 				// aspectRatio: NaN,
+	// 				crop: function (event) {
+	// 					demo1.innerHTML =
+	// 						"<strong>Width:</strong> " +
+	// 						Math.round(event.detail.width) +
+	// 						"px" +
+	// 						" <strong>Height:</strong> " +
+	// 						Math.round(event.detail.height) +
+	// 						"px";
+	// 				},
+	// 			});
+	// 		};
+	// 		reader.readAsDataURL(file);
+	// 	}
+	// 	main_container.style.display = "block";
+	// });
 
 	function updateAspectRatio() {
 		const cropBoxData = cropper.getCropBoxData();
